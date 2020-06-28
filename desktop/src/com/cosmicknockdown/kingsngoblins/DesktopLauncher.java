0209140feefd303381f1a7d6f269bd954ced6b73
ArtSuh <artyom.suhov@gmail.com>
2020-06-28 20:59:48 +0300

    add assets folder

--- desktop/src/com/cosmicknockdown/kingsngoblins/DesktopLauncher.java
+++ desktop/src/com/cosmicknockdown/kingsngoblins/DesktopLauncher.java
@@ -5,13 +5,9 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class DesktopLauncher {
     public static void main(final String[] arg) {
-
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.width = 16 * 48;
         config.height = 16 * 43;
         new LwjglApplication(new KNG(), config);
-
     }
-
-
 }
